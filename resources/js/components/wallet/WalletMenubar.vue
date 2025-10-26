6fd3517f090caec7e92938d90bc2a1f2606499da
g-i-kala <5star@wp.pl>
2025-10-26 14:26:28 +0100

    refactor: navmenu implemented

--- resources/js/components/wallet/WalletMenubar.vue
+++ resources/js/components/wallet/WalletMenubar.vue
@@ -1,51 +1,48 @@
 <script setup lang="ts">
 import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from '@/components/ui/menubar'
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
+import { Link } from '@inertiajs/vue3'
+
 </script>
 
 <template>
-  <Menubar>
-    <MenubarMenu>
-      <MenubarTrigger>ETF</MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem :as="'a'" href="/wallet/etf">
-          Overview
-        </MenubarItem>
-        
-      </MenubarContent>      
-    </MenubarMenu>
-  <MenubarMenu>
-    <MenubarTrigger>Bonds</MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
-          New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-        </MenubarItem>
-        <MenubarItem>New Window</MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>Share</MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>Print</MenubarItem>
-      </MenubarContent>
-    </MenubarMenu>
-    <MenubarMenu>
-      <MenubarTrigger>Stocks</MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
-          New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-        </MenubarItem>
-        <MenubarItem>New Window</MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>Share</MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>Print</MenubarItem>
-      </MenubarContent>
-    </MenubarMenu>
-  </Menubar>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>ETF</NavigationMenuTrigger>
+        <NavigationMenuContent>
+            <NavigationMenuLink asChild>
+            <Link href="/wallet/etf">Overview</Link>
+          </NavigationMenuLink>
+          <NavigationMenuLink asChild>
+            <Link href="/wallet/etf">My List</Link>
+          </NavigationMenuLink>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+       <NavigationMenuItem>
+        <NavigationMenuTrigger>Bonds</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuLink asChild>
+            <Link href="/wallet/bonds">Overview</Link>
+          </NavigationMenuLink>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+       <NavigationMenuItem>
+        <NavigationMenuTrigger>Stocks</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuLink asChild>
+            <Link href="/wallet/stocks">Overview</Link>
+          </NavigationMenuLink>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
 </template>
